0c68a54ce39d2fb49ac3bb984d2e611ac84ad1ba
Gilbert Marpuri <gilbertmarpuri@artistryware.com>
2018-08-16 21:42:18 +0800

    first

---
+++ Insta Detective/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Insta Detective
+//
+//  Created by Gilbert Marpuri on 16/08/2018.
+//  Copyright © 2018 Gilbert Marpuri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func start(_ sender: UIButton) {
+        print(1);
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
